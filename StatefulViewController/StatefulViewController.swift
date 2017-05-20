81221f37b14bfa7c491afa0a13199cc4251e2b6c
Wolfgang Lutz <wlut@num42.de>
2017-05-20 09:54:47 +0200

    fix typos using misspell

--- StatefulViewController/StatefulViewController.swift
+++ StatefulViewController/StatefulViewController.swift
@@ -57,18 +57,18 @@ public protocol StatefulViewController: class, BackingViewProvider {
     func startLoading(animated: Bool, completion: (() -> Void)?)
     
     /// Ends the controller's loading state.
-    /// If an error occured, the error view is shown.
+    /// If an error occurred, the error view is shown.
     /// If the `hasContent` method returns false after calling this method, the empty view is shown.
     ///
     /// - parameter animated: 	true if the switch to the placeholder view should be animated, false otherwise
-    /// - parameter error:		An error that might have occured whilst loading
+    /// - parameter error:		An error that might have occurred whilst loading
     func endLoading(animated: Bool, error: Error?, completion: (() -> Void)?)
     
     /// Transitions the view to the appropriate state based on the `loading` and `error`
     /// input parameters and shows/hides corresponding placeholder views.
     ///
     /// - parameter loading:		true if the controller is currently loading
-    /// - parameter error:		An error that might have occured whilst loading
+    /// - parameter error:		An error that might have occurred whilst loading
     /// - parameter animated:	true if the switch to the placeholder view should be animated, false otherwise
     func transitionViewStates(loading: Bool, error: Error?, animated: Bool, completion: (() -> Void)?)
     
@@ -80,10 +80,10 @@ public protocol StatefulViewController: class, BackingViewProvider {
     /// - returns: true if there is content available in your controller.
     func hasContent() -> Bool
     
-    /// This method is called if an error occured, but `hasContent` returned true.
+    /// This method is called if an error occurred, but `hasContent` returned true.
     /// You would typically display an unobstrusive error message that is easily dismissable
     /// for the user to continue browsing content.
     ///
-    /// - parameter error:	The error that occured
+    /// - parameter error:	The error that occurred
     func handleErrorWhenContentAvailable(_ error: Error)
 }
